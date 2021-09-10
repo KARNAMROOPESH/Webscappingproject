7beb4b7b3db8db670e1a5a4bdb5090c70f9926cc
Karnam Roopesh <63301556+KARNAMROOPESH@users.noreply.github.com>
2021-09-10 19:53:45 +0530

    Add files via upload

---
+++ scapperweb2.py
@@ -0,0 +1,39 @@
+import time
+import csv
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+import pandas as pd
+
+DATAURL = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+page = requests.get(DATAURL)
+
+soup = BeautifulSoup(page.text,'html.parser')
+
+tables = soup.find_all('table')
+
+temp = []
+
+tablerows = tables[3].find_all('tr')
+
+for tr in tablerows :
+
+    tdtag = tr.find_all('td')
+    row = [i.text.rstrip() for i in tdtag]
+    temp.append(row)
+
+name = []
+dist = []
+mass = []
+radius = []
+
+for i in range(1,len(temp)):
+    name.append(temp[i][0])
+    dist.append(temp[i][5])
+    mass.append(temp[i][8])
+    radius.append(temp[i][9])
+
+df = pd.DataFrame(list(zip(name , dist , mass , radius)) , columns = ["name","distance","mass","radius"] )
+
+df.to_csv('data2.csv')
